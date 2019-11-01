bc6e5223eb9dd3e602c4a4c1ec89372d2cca403d
wazedrifat <34921453+wazedrifat@users.noreply.github.com>
2019-11-01 16:12:08 +0600

    fake developer

---
+++ inputModify.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IN freopen("input.txt", "r", stdin);
+#define OUT freopen("in.txt", "w", stdout);
+#define ll long long int
+#define PII pair <int, int>
+#define MX 100001
+
+vector <string> v;
+
+int main() 
+{
+	IN
+	OUT
+
+	for (int i = 0; i < 49869; i++) {
+		string s;
+		v.clear();
+		int cnt = 0;
+
+		while (cin >> s && s != "$")
+			v.push_back(s);
+
+		cout << v.size() << " ";
+		for (int i = 0; i < v.size(); i++)
+			cout << v[i] << " ";
+		cout << endl;
+	}
+}
