64c40b6fbe8b3c5f1442f54429779c7ddf8da381
wazedrifat <34921453+wazedrifat@users.noreply.github.com>
2019-11-02 06:53:54 +0600

    1

--- temp.cpp
+++ temp.cpp
@@ -2,275 +2,29 @@
 using namespace std;
 
 #define IN freopen("in.txt", "r", stdin);
-#define OUT freopen("graph.txt", "w", stdout);
+#define OUT freopen("out.txt", "w", stdout);
 #define ll long long int
-#define PII pair <ll, ll>
-#define PDD pair <double, double>
-#define DhakaRoadMX 49869
-#define BikolpoBusRoadMX 17
-#define UttaraBusRoadMX 19
-#define MetroRailMX 14
-#define NodeMX 1500
-#define EdgeMX NodeMX * NodeMX
-#define PI 2.0 * acos(0.0)
-#define modify 1000000
+#define PII pair <int, int>
+#define MX 100001
 
-const double typeCost[] = {1.0, 1.0, 1.0};
+int main()
+{
+	int a = 12;
+	int b = a/5, c = a%6;
 
-// car = 0
-// metro = 1
-// bikolpo bus = 2
-//uttara bus = 2
+	stringstream ss;
+	string s1, s2;
 
+	ss << b;
+	ss >> s1;
 
-vector <ll> g[NodeMX], type[NodeMX];
-vector <double> cost[NodeMX];
-map <PII, ll> road;
-PII RoadList[NodeMX];
-ll path[NodeMX], pathType[NodeMX];
-double dist[NodeMX];
+	ss << c;
+	ss >> s2;
 
-void Dijkstra(ll source) {
-	map<double, ll> m;
+	cout << "s1.length() = " << s1.length() << endl;		//ThisIsForDebuggingPurposes
+	cout << "s2.length() = " << s2.length() << endl;		//ThisIsForDebuggingPurposes
 	
-	for (ll i = 1; i < NodeMX; i++)
-	{
-		dist[i] = __LONG_LONG_MAX__;
-		path[i] = -1;
-		pathType[i] = -1;
-	}
+	string res = s1 + " , " + s2;
 
-	m[0.0] = source;
-	dist[source] = 0.0;
-
-	// cout << "cleared" << endl;
-	while (!m.empty())
-	{
-		// cout << "m.size() = " << m.size() << endl;		//ThisIsForDebuggingPurposes
-		map<double, ll>::iterator it = m.begin();
-
-		ll u = it->second;
-		m.erase(it);
-
-		for (ll i = 0; i < g[u].size(); i++)
-		{
-			ll v = g[u][i];
-			ll pathTP = type[u][i];
-			double NewCost = dist[u] + cost[u][i];
-
-			if (NewCost < dist[v])
-			{
-				path[v] = u;
-				pathType[v] = pathTP;
-				dist[v] = NewCost;
-				m[NewCost] = v;
-			}
-		}
-	}
-}
-
-void PrintPath(ll v) {
-	if (v == -1)
-		return;
-	PrintPath(path[v]);
-	cout << v << " ";
-}
-
-double deg2rad(double deg) {
-	return deg * (PI / 180.0);
-}
-
-double square(double n) {
-	return n * n;
-}
-
-ll error = 0;
-
-PDD revertPII(PII a) {
-	double x = (double)a.first / modify;
-	double y = (double)a.second / modify;
-	return PDD(x, y);
-}
-
-double distance(PII A, PII B) {
-	PDD a = revertPII(A);
-	PDD b = revertPII(B);
-
-	double R = 6371.0;
-	double dLat = deg2rad(a.first - b.first);
-	double dLon = deg2rad(a.second - b.second);
-
-	double c = square(sin(dLat / 2.0)) + cos(deg2rad(a.first)) * cos(deg2rad(b.first)) * square(sin(dLon / 2.0));
-	double d = 2.0 * atan( sqrt(c) / sqrt(1.0 - c));
-
-	if (A == B) {
-		cout << "error : " << A.first << " " << B.first << " " << A.second << " " << B.second <<
-		error++;
-	}
-	else if (d == 0.0) {
-		cout << "zero : " << road[a] << " " << road[b] << endl; 
-		cout << "c = " << c << endl;		//ThisIsForDebuggingPurposes
-		cout << "d = " << d << endl;		//ThisIsForDebuggingPurposes
-		cout << "dLat = " << dLat << endl;		//ThisIsForDebuggingPurposes
-		cout << "dLon = " << dLon << endl;		//ThisIsForDebuggingPurposes
-		cout << endl << endl << endl;		//ThisIsForDebuggingPurposes
-	}
-	
-
-	return R * d;
-}
-
-ll err[3];
-int work = 0;
-
-ll str (string s) {
-	ll res = 0;
-
-	for (int i = 0; i <s.length(); i++) {
-		
-		if (s[i] == '.')
-			continue;
-
-		ll digit = s[i] - '0';
-
-		res += digit;
-		res *= 10;
-	}
-
-	for (int i = s.length(); i <= 12; i++) 
-		res *= 10;
-
-	return res;
-}
-
-ll scan (ll limit, ll vehicle, ll RoadNo) {
-	for (ll i = 0; i < limit; i++) {
-		int n;
-		scanf("%d", &n);
-
-		ll prevRoadNo = 0, curRoadNo = 0;
-		ll preva, prevb;
-		for (ll j = 0; j < n; j++){
-			// double A, B;
-			// scanf("%lf%lf", &A, &B);
-
-			string A, B;
-			cin >> A >> B;
-
-			ll a = str(A);
-			ll b = str(B);
-
-			// cout << "a = " << a << endl;		//ThisIsForDebuggingPurposes
-			// cout << "A = " << A << endl;		//ThisIsForDebuggingPurposes
-			// cout << "str(A) = " << str(A) << endl;		//ThisIsForDebuggingPurposes
-			// work += (a%10 > 0) + (b%10 > 0);
-
-			// if (a%10 > 0)
-			// 	cout << "work = " << A << " " << a << " " << endl;
-
-			if (j && a == preva && b == prevb) {
-				// cout << "vehicle = " << vehicle << endl;		//ThisIsForDebuggingPurposes
-				// // cout << " = " <<  << endl;		//ThisIsForDebuggingPurposes
-				err[vehicle]++;
-			}
-
-			if (road.find( PII(a, b) ) != road.end()) {
-				curRoadNo = road[ PII(a, b) ];
-			}
-			else {
-				road[ PII(a, b) ] = ++RoadNo;
-				curRoadNo = RoadNo;
-				RoadList[RoadNo] = PII(a, b);
-			}
-
-			if (j) {
-				g[curRoadNo].push_back(prevRoadNo);
-				g[prevRoadNo].push_back(curRoadNo);
-
-				type[curRoadNo].push_back(vehicle);
-				type[prevRoadNo].push_back(vehicle);
-
-				double dist = distance(RoadList[curRoadNo], RoadList[prevRoadNo]) * typeCost[vehicle];
-				// ll distLL = (ll)(dist * modify);
-				cost[curRoadNo].push_back(dist);
-				cost[prevRoadNo].push_back(dist);
-			}
-			prevRoadNo = curRoadNo;
-
-			preva = a;	prevb = b;
-		}
-	}
-	return RoadNo;
-}
-
-int main() {
-	OUT
-
-	ll RoadNo = 0;
-
-	freopen("Roadmap_Dhaka.txt", "r", stdin);
-	RoadNo = scan(DhakaRoadMX, 0, RoadNo);
-	
-	ll r1 = RoadNo;
-	cout << "read dhaka road" << endl;
-	cout << "RoadNo = " << RoadNo << endl;		//ThisIsForDebuggingPurposes
-
-	freopen("Roadmap_DhakaMetroRail.txt", "r", stdin);
-	RoadNo = scan(MetroRailMX, 1, RoadNo);
-
-	ll r2 = RoadNo;
-	cout << "read Metro Rail" << endl;
-	cout << "RoadNo = " << r2 - r1 << endl;		//ThisIsForDebuggingPurposes
-
-	freopen("Roadmap_BikolpoBus.txt", "r", stdin);
-	RoadNo = scan(BikolpoBusRoadMX, 2, RoadNo);
-
-	ll r3 = RoadNo;
-	cout << "read Uttara Bus" << endl;
-	cout << "RoadNo = " << r3 - r2 << endl;		//ThisIsForDebuggingPurposes
-
-	freopen("Roadmap_UttaraBus.txt", "r", stdin);
-	RoadNo = scan(UttaraBusRoadMX, 2, RoadNo);
-
-	ll r4 = RoadNo;
-	cout << "read Bikolpo Bus" << endl;
-	cout << "RoadNo = " << r4 - r3 << endl;		//ThisIsForDebuggingPurposes
-
-	cout << "error = " << error << endl;		//ThisIsForDebuggingPurposes
-
-	cout << "err : " << err[0] << " " << err[1] << " " << err[2] << endl;
-
-	cout << "work = " << work << endl;		//ThisIsForDebuggingPurposes
-
-	
-	for (ll i = 0 ; i < 50; i++) {
-		for (ll j = 0; j < g[i].size(); j++) {
-			cout << i << " " << j << " " << cost[i][j] << endl;
-		}
-	}
-
-	// freopen("in.txt", "r", stdin);
-	// double a, b, c, d;
-	// scanf("%lf%lf %lf%lf", &a, &b, &c, &d);
-
-	ll source = 2, destination = 19;
-
-	cout << "before" << endl;
-	Dijkstra(2);
-
-	cout << "dist[19] = " << dist[19] << endl;		//ThisIsForDebuggingPurposes
-
-	for (ll i = 0; i < 20; i++) {
-		cout << "i = " << i << " dis = " << dist[i] << endl;
-	}
-	cout << endl << endl << endl;		//ThisIsForDebuggingPurposes
-
-	for (ll i = 0; i < 20; i++) {
-		cout << "i = " << i << " path = " << path[i] << endl;
-	}
-	
-	cout << endl << endl << endl;		//ThisIsForDebuggingPurposes
-
-	cout << "path : ";
-	PrintPath(destination);
+	cout << res << endl;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,195 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IN freopen("in.txt", "r", stdin);
+#define OUT freopen("out.txt", "w", stdout);
+#define ll long long int
+#define PII pair <double, double>
+#define DhakaRoadMX 50000
+#define NodeMX 5 * DhakaRoadMX
+#define PI 2.0 * acos(0.0)
+#define EPS 1e-10
+
+const double typeCost[] = {20.0, 5.0, 7.0};
+const double multiply = 1000000.0;
+const double speed = 10.0;
+const int interval = 15;
+
+// car = 0
+// metro = 1
+// bikolpo bus = 2
+//uttara bus = 2
+// walk = 3
+//wait = 4
+
+
+int flag[2];
+int wait[NodeMX];
+vector <int> g[NodeMX], type[NodeMX];
+vector <PII> points;
+vector <ll>  cost[NodeMX];
+map <PII, int> road;
+PII RoadList[NodeMX];
+int path[NodeMX], pathType[NodeMX];
+ll dist[NodeMX];
+map <ll, int> m;
+
+int getTime (int hour, int minute, int type) {
+	return ((hour % 12) * 60 + minute) + (type * 12 * 60);
+}
+
+string getFormattedTime(int time) {
+	string tp, s1, s2;
+
+	if (time >= 12 * 60) {
+		time -= 12 * 60;
+	}
+	else
+		tp = "AM";
+
+	stringstream ss;
+	ss << (time/60);
+	ss >> s1;
+
+	ss << (time%60);
+	ss >> s2;
+
+	if (s1.length() == 0)
+		s1 += '0';
+	
+	if (s2.length() == 0)
+		s2 += '0';
+	
+	string res = s1 + ":" + s2 + " " + tp;
+	return res;
+}
+
+int startingTime = getTime(5, 43, 0);
+
+void Dijkstra(int source) {
+	for (int i = 1; i < NodeMX; i++)
+	{
+		dist[i] = LONG_LONG_MAX;
+		path[i] = -1;
+		pathType[i] = -1;
+	}
+
+	m[startingTime] = source;
+	dist[source] = startingTime;
+
+
+	while (!m.empty()) {
+		map<ll, int>::iterator it = m.begin();
+
+		int u = it->second;
+		m.erase(it);
+
+		for (int i = 0; i < g[u].size(); i++)
+		{
+			int v = g[u][i];
+			int pathTP = type[u][i];
+			ll NewCost = (ll)(dist[u] + cost[u][i]);
+			
+			if (dist[u] < 6 * 12) {
+				NewCost += 6 * 12 - dist[u];
+			}
+			else if (dist[u] > 23 * 12) {
+				NewCost = LONG_LONG_MAX;
+			}
+
+			if (type[u][i] == 1 || type[u][i] == 2) {
+				NewCost += (interval - (dist[u] % interval)) % interval;
+			}
+
+			if (NewCost < dist[v])
+			{
+				path[v] = u;
+				pathType[v] = pathTP;
+				dist[v] = NewCost;
+				m[NewCost] = v;
+				wait[v] = (interval - (dist[u] % interval)) % interval;
+			}
+		}
+	}
+}
+
+int PrintPath(int v) {
+	if (v == -1)
+		return 1;
+	
+	int val = PrintPath(path[v]);
+
+	if (val == 2)
+		cout << pathType[v] << endl;
+	
+	if (wait[v] > 0)
+		cout << "wait " << getFormattedTime(wait[v] / (int)multiply) << " minutes" << endl;
+
+	cout << setprecision(7) << fixed << RoadList[v].first << "," << setprecision(7) << fixed << RoadList[v].second << "," << getFormattedTime(dist[v] / (int)multiply) ;
+
+	if (val == 1)
+		return 2;
+	else
+		cout << pathType[v] << endl;
+		
+
+	return 0;
+}
+
+double deg2rad(double deg) {
+	return deg * (PI / 180.0);
+}
+
+double square(double n) {
+	return n * n;
+}
+
+double distance(PII a, PII b) {
+	double R = 6371.0;
+	double dLat = deg2rad(a.first - b.first);
+	double dLon = deg2rad(a.second - b.second);
+
+	double c = square(sin(dLat / 2.0)) + cos(deg2rad(a.first)) * cos(deg2rad(b.first)) * square(sin(dLon / 2.0));
+	double d = 2.0 * atan( sqrt(c) / sqrt(1.0 - c));
+	return R * d;
+}
+
+int scan (int vehicle, int RoadNo) {
+	int n;
+	while (scanf("%d", &n) != EOF) {
+		n /= 2;
+
+		int prevRoadNo = -1, curRoadNo = -2;
+		for (int j = 0; j < n; j++){
+			double a, b;
+			scanf("%lf%lf", &a, &b);
+
+			if (road.find( PII(a, b) ) != road.end()) {
+				curRoadNo = road[ PII(a, b) ];
+			}
+			else {
+				road[ PII(a, b) ] = ++RoadNo;
+				curRoadNo = RoadNo;
+				points.push_back( PII(a, b) );
+				RoadList[RoadNo] = PII(a, b);
+			}
+
+			if (j) {
+				g[curRoadNo].push_back(prevRoadNo);
+				g[prevRoadNo].push_back(curRoadNo);
+
+				type[curRoadNo].push_back(vehicle);
+				type[prevRoadNo].push_back(vehicle);
+
+				double dist = distance(RoadList[curRoadNo], RoadList[prevRoadNo]) * speed * multiply;
+				cost[curRoadNo].push_back(dist);
+				cost[prevRoadNo].push_back(dist);
+			}
+			prevRoadNo = curRoadNo;
+		}
+	}
+	return RoadNo;
+}
 
 PII cmpPair;
 bool cmp(PII a, PII b) {
@@ -5,4 +197,65 @@ bool cmp(PII a, PII b) {
 	double d2 = distance (b, cmpPair) * multiply;
 
 	return (d1 - d2 <= EPS);
+}
+
+int findPoint(PII p, int n) {
+	if (road.find(p) != road.end())
+		return road[p];
+
+	flag[n] = 0;
+	cmpPair = p;
+	sort(points.begin(), points.end(), cmp);
+	startingTime += distance(points[0], p) * speed;
+	return road[ points[0] ];
+}
+
+int main() {
+	
+
+	freopen("Roadmap_Dhaka.txt", "r", stdin);
+	int RoadNo = scan(0, 0);
+
+	freopen("Roadmap_DhakaMetroRail.txt", "r", stdin);
+	RoadNo = scan(1, RoadNo);
+
+	freopen("Roadmap_BikolpoBus.txt", "r", stdin);
+	RoadNo = scan(2, RoadNo);
+
+	freopen("Roadmap_UttaraBus.txt", "r", stdin);
+	RoadNo = scan(2, RoadNo);
+	
+	freopen("input.txt", "r", stdin);
+	double a, b, c, d;
+	int h, m;
+	string s;
+	scanf("%lf%lf %lf%lf", &a, &b, &c, &d);
+	cin >> h >> m >> s;
+
+	cout << "h = " << h << endl;		//ThisIsForDebuggingPurposes
+	cout << "m = " << m << endl;		//ThisIsForDebuggingPurposes
+	cout << "s = " << s << endl;		//ThisIsForDebuggingPurposes
+
+	startingTime = getTime(a, b, (s == "PM"));
+	// double a = 90.364255;
+	// double b = 23.828335;
+	// double c = 90.36315;
+	// double d = 23.804896;
+	flag[0] = flag[1] = 1;
+	int source = findPoint(PII(a, b), 0), destination = findPoint(PII(c, d), 1);
+	
+	cout << "source = " << source << endl;		//ThisIsForDebuggingPurposes
+	cout << "destination = " << destination << endl;		//ThisIsForDebuggingPurposes
+
+	Dijkstra(source);
+
+	freopen("output.txt", "w", stdout);
+
+	if (!flag[0])
+		cout << setprecision(7) << fixed << a << "," << setprecision(7) << fixed << b << ",3" << endl;
+	
+	PrintPath(destination);
+
+	if (!flag[1])
+		cout << setprecision(7) << fixed << c << "," << setprecision(7) << fixed << d << ",3" << endl;
 }
